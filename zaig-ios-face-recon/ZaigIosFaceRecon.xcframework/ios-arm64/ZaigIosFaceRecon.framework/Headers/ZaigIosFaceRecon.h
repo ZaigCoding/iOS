14acb4e09c0cd53f858dbcbf22df3d194629c252
Gabriel Scherer <gabriel.scherer@zaig.com.br>
2021-02-26 20:46:31 -0300

    Adding ZaigIosFaceRecon v1.1.0

---
+++ zaig-ios-face-recon/ZaigIosFaceRecon.xcframework/ios-arm64/ZaigIosFaceRecon.framework/Headers/ZaigIosFaceRecon.h
@@ -0,0 +1,18 @@
+//
+//  ZaigIosFaceRecon.h
+//  ZaigIosFaceRecon
+//
+//  Created by Gabriel Scherer Schwening on 17/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZaigIosFaceRecon.
+FOUNDATION_EXPORT double ZaigIosFaceReconVersionNumber;
+
+//! Project version string for ZaigIosFaceRecon.
+FOUNDATION_EXPORT const unsigned char ZaigIosFaceReconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZaigIosFaceRecon/PublicHeader.h>
+
+
