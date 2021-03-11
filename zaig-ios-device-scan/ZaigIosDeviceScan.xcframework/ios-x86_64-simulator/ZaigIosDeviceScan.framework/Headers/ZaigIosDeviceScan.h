73072fef3db7255d594f40b855e0c0ea9b9be5e1
Gabriel Scherer <gabriel.scherer@zaig.com.br>
2021-03-11 13:05:25 -0300

    Adding ZaigIosDeviceScan v1.2.0

---
+++ zaig-ios-device-scan/ZaigIosDeviceScan.xcframework/ios-x86_64-simulator/ZaigIosDeviceScan.framework/Headers/ZaigIosDeviceScan.h
@@ -0,0 +1,18 @@
+//
+//  ZaigIosDeviceScan.h
+//  ZaigIosDeviceScan
+//
+//  Created by Gabriel Scherer Schwening on 26/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZaigIosDeviceScan.
+FOUNDATION_EXPORT double ZaigIosDeviceScanVersionNumber;
+
+//! Project version string for ZaigIosDeviceScan.
+FOUNDATION_EXPORT const unsigned char ZaigIosDeviceScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZaigIosDeviceScan/PublicHeader.h>
+
+
